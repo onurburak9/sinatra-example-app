0cab8ab45f131241a2a187dface25792c825fee1
Onur <onurburak@sabanciuniv.edu>
2017-11-11 18:43:08 -0800

    initial commit

---
+++ server.rb
@@ -0,0 +1,139 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "dm-core"
+require "dm-migrations" 
+
+def active_page?(path='')
+  request.path_info == '/' + path
+end
+
+DataMapper::Logger.new($stdout, :debug) 
+DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}users.db")
+DataMapper.setup(:students, "sqlite3:///#{Dir.pwd}students.db")
+DataMapper.setup(:comments, "sqlite3:///#{Dir.pwd}comments.db")
+
+
+#DB table definitions
+class Student
+	include DataMapper::Resource 
+	property :id,   Serial  
+	property :firstname, String, :required => true
+	property :surname, String, :required => true
+end
+
+class Comment
+	include DataMapper::Resource 
+	property :id,   Serial  
+	property :ownername, String, :required => true
+	property :content, String, :required => true
+	property :created_at, DateTime
+end
+	DataMapper.finalize.auto_upgrade!
+
+get '/hello' do 
+	"<h1>hello, this is my first sinatra web application<h1>"
+end
+#routes for HTML pages
+get "/" do
+	@title = 'Home'
+	erb :home
+end
+
+#routes for Students
+get '/students' do
+	@title = 'Students'
+	@students = Student.all
+	erb :students
+end
+
+get '/student/:id' do |id|
+	@student = Student.get!(id)
+
+	erb :student_details
+end
+
+get '/students/new' do
+	@title = 'Students'
+	erb :add_student
+end
+
+post '/student/add' do
+  	student = Student.new(firstname:params["name"], surname:params["surname"])
+
+  	if student.save
+		redirect '/students'
+	else
+		redirect '/students/new'
+	end
+end
+
+put '/student/:id' do |id|
+	student = Student.get!(id)
+	changedStudent = student.update!(firstname:params["name"], surname:params["surname"])
+	  
+	if changedStudent
+	   redirect "/students"
+	else
+	   redirect "/student/#{id}"
+	end
+end
+
+delete '/student/:id' do |id|
+	student = Student.get!(id)
+	student.destroy!
+	redirect "/students"
+end
+
+#routes for Comments
+get '/comments' do
+	@title = 'Comments'
+	@comments = Comment.all
+
+	p @comments 
+	erb :comments
+end
+
+get '/comment/:id' do |id|
+	@comment = Comment.get!(id)
+
+	erb :comment_details
+	
+end
+
+post '/comment/add' do
+	comment = Comment.new(ownername:params["ownername"], content:params["content"],created_at:Time.now)
+
+	if comment.save
+		redirect '/comments'
+	end
+	
+end
+
+delete '/comment/:id' do |id|
+	comment = Comment.get!(id)
+	comment.destroy!
+	redirect "/comments"
+	
+end
+
+#route for Video
+get '/video' do
+	erb :video
+end
+
+#routes for Login
+get '/login' do
+	erb :home, :layout => :login_layout
+end
+
+post '/login' do
+	username = params[:username]
+	password = params[:password] 
+end
+
+get '/*' do
+   "<h1>404, Wrong Request</h1>" 
+end
+
+
+
